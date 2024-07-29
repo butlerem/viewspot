34c2d6b76ffd275bb22fd009d452e57123d18153
butlerem <butlerem01@gmail.com>
2024-07-29 06:32:31 -0400

    import ui libraries

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,109 @@
-import React from 'react'
+"use client"
+import { Button } from '@/components/ui/button';
+import { Separator } from '@/components/ui/separator';
+import { Camera, Divide, FlipHorizontal, MoonIcon, PersonStanding, SunIcon, Video, Volume2 } from 'lucide-react';
+import React, { useEffect, useRef, useState } from 'react'
+import Webcam from 'react-webcam';
+import { toast } from "sonner"
+import "@tensorflow/tfjs-backend-cpu"
+import "@tensorflow/tfjs-backend-webgl"
 
 type Props = {}
 
+let interval: any = null;
+let stopTimeout: any = null;
 const HomePage = (props: Props) => {
-  return (
-    <div>Hi world</div>
-  )
-}
+  const webcamRef = useRef<Webcam>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
 
-export default HomePage
+  // state 
+  const [mirrored, setMirrored] = useState<boolean>(true);
+  const [isRecording, setIsRecording] = useState<boolean>(false);
+  const [autoRecordEnabled, setAutoRecordEnabled] = useState<boolean>(false)
+  const [volume, setVolume] = useState(0.8);
+  const [loading, setLoading] = useState(false);
+
+  const mediaRecorderRef = useRef<MediaRecorder | null>(null);
+
+  // initialize media recorder
+  useEffect(() => {
+    if (webcamRef && webcamRef.current) {
+      const stream = (webcamRef.current.video as any).captureStream();
+      if (stream) {
+        mediaRecorderRef.current = new MediaRecorder(stream);
+
+        mediaRecorderRef.current.ondataavailable = (e) => {
+          if (e.data.size > 0) {
+            const recordedBlob = new Blob([e.data], { type: 'video' });
+            const videoURL = URL.createObjectURL(recordedBlob);
+
+            const a = document.createElement('a');
+            a.href = videoURL;
+            a.download = `${formatDate(new Date())}.webm`;
+            a.click();
+          }
+        };
+        mediaRecorderRef.current.onstart = (e) => {
+          setIsRecording(true);
+        }
+        mediaRecorderRef.current.onstop = (e) => {
+          setIsRecording(false);
+        }
+      }
+    }
+  }, [webcamRef])
+
+
+  useEffect(() => {
+    setLoading(true);
+    initModel();
+  }, [])
+
+  // load model 
+  // set the model in a state varaible
+  async function initModel() {
+    const loadedModel: ObjectDetection = await cocossd.load({
+      base: 'mobilenet_v2'
+    });
+    setModel(loadedModel);
+  }
+
+  useEffect(() => {
+    if (model) {
+      setLoading(false);
+    }
+  }, [model])
+
+
+  async function runPrediction() {
+    if (
+      model
+      && webcamRef.current
+      && webcamRef.current.video
+      && webcamRef.current.video.readyState === 4
+    ) {
+      const predictions: DetectedObject[] = await model.detect(webcamRef.current.video);
+
+      resizeCanvas(canvasRef, webcamRef);
+      drawOnCanvas(mirrored, predictions, canvasRef.current?.getContext('2d'))
+
+      let isPerson: boolean = false;
+      if (predictions.length > 0) {
+        predictions.forEach((prediction) => {
+          isPerson = prediction.class === 'person';
+        })
+
+        if (isPerson && autoRecordEnabled) {
+          startRecording(true);
+        }
+      }
+    }
+  }
+
+  useEffect(() => {
+    interval = setInterval(() => {
+      runPrediction();
+    }, 100)
+
+    return () => clearInterval(interval);
+  }, [webcamRef.current, model, mirrored, autoRecordEnabled])
